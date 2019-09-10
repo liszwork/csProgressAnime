dc2611cbfd50baebbbecaf6a5e5538e901a2d313
lisns <z.z.iz.0pnup@gmail.com>
2019-09-10 09:16:52 +0900

    処理中アニメーションのサンプル

---
+++ TestProgressAnimation/Form1.Designer.cs
@@ -0,0 +1,109 @@
+﻿namespace TestProgressAnimation
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必要なデザイナー変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージド リソースを破棄する場合は true を指定し、その他の場合は false を指定します。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if ( disposing && (components != null) )
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows フォーム デザイナーで生成されたコード
+
+        /// <summary>
+        /// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+        /// コード エディターで変更しないでください。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.pictureProgress = new System.Windows.Forms.PictureBox();
+            this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureProgress)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(111, 83);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Run";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(143, 12);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 12);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "label1";
+            // 
+            // pictureProgress
+            // 
+            this.pictureProgress.Image = ((System.Drawing.Image)(resources.GetObject("pictureProgress.Image")));
+            this.pictureProgress.Location = new System.Drawing.Point(254, 12);
+            this.pictureProgress.Name = "pictureProgress";
+            this.pictureProgress.Size = new System.Drawing.Size(61, 54);
+            this.pictureProgress.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureProgress.TabIndex = 36;
+            this.pictureProgress.TabStop = false;
+            // 
+            // backgroundWorker1
+            // 
+            this.backgroundWorker1.DoWork += new System.ComponentModel.DoWorkEventHandler(this.backgroundWorker1_DoWork);
+            this.backgroundWorker1.ProgressChanged += new System.ComponentModel.ProgressChangedEventHandler(this.backgroundWorker1_ProgressChanged);
+            this.backgroundWorker1.RunWorkerCompleted += new System.ComponentModel.RunWorkerCompletedEventHandler(this.backgroundWorker1_RunWorkerCompleted);
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(145, 72);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(170, 23);
+            this.progressBar1.TabIndex = 37;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(327, 105);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.pictureProgress);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureProgress)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.PictureBox pictureProgress;
+        private System.ComponentModel.BackgroundWorker backgroundWorker1;
+        private System.Windows.Forms.ProgressBar progressBar1;
+    }
+}
+
---
+++ TestProgressAnimation/Form1.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TestProgressAnimation
+{
+    public partial class Form1 : Form
+    {
+        private int cnt = 0;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            this.StopTextLabel();
+            this.VisibleProgressImage(false);
+            // スレッドからの進捗通知を可能にする(これはデザイナでも変更可能)
+            this.backgroundWorker1.WorkerReportsProgress = true;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            this.button1.Enabled = false;
+            this.StopTextLabel();
+            this.VisibleProgressImage(true);
+            this.progressBar1.Value = 0;
+            this.backgroundWorker1.RunWorkerAsync();
+        }
+
+        private void StopTextLabel() => this.label1.Text = "停止中";
+        private void VisibleProgressImage(bool isVisible) => pictureProgress.Visible = isVisible;
+
+        private void Processing()
+        {
+            Thread.Sleep(100);
+        }
+
+        // 別スレッドでの動作
+        private void backgroundWorker1_DoWork(object sender, DoWorkEventArgs e)
+        {
+            for ( int i = 1; i <= 100; i++ )
+            {
+                this.cnt = i;
+                this.Processing();
+
+                int progress = this.cnt;
+                string msg = "実行中... ";
+                this.backgroundWorker1.ReportProgress(progress, msg);
+            }
+        }
+
+        // スレッドの進捗を受け取る
+        private void backgroundWorker1_ProgressChanged(object sender, ProgressChangedEventArgs e)
+        {
+            this.label1.Text = (string)e.UserState + " " + e.ProgressPercentage.ToString() + "%";
+            this.progressBar1.Value = e.ProgressPercentage;
+        }
+
+        // スレッド完了通知を受け取る
+        private void backgroundWorker1_RunWorkerCompleted(object sender, RunWorkerCompletedEventArgs e)
+        {
+            MessageBox.Show("処理完了！");
+            this.VisibleProgressImage(false);
+            this.StopTextLabel();
+            this.button1.Enabled = true;
+        }
+
+    }
+}
